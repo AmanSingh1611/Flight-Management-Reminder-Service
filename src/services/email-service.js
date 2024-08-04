bcb31f087e81ddc13a2606bc4e54a621d8bf0460
Aman <aman16024375@gmail.com>
2024-08-05 01:12:01 +0530

    Feat: Added Notification Cron

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,4 +1,7 @@
 const sender = require('../config/emailConfig');
+const TicketRepository = require('../repository/ticket-repository');
+
+const repo = new TicketRepository();
 
 const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     try {
@@ -12,11 +15,42 @@ const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     } catch (error) {
         console.log(error);
     }
-    
+}
+
+const fetchPendingEmails = async (timestamp) => {
+    try {
+        const response = await repo.get({status: "PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateTicket = async (ticketId, data) => {
+    try {
+        const response = await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const createNotification = async (data) => {
+    try {
+        console.log(data);
+        const response = await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
-    sendBasicEmail
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification,
+    updateTicket
 }
 
 
